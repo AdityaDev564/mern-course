a74b006a37cedcb679a5330b5b27fe91fe80e29b
Aditya Dev <adityadev564@gmail.com>
2024-07-04 22:20:44 +0530

    added starter frontend components

---
+++ frontend/src/components/DashFooter.js
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const DashFooter = () => {
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+
+    const onGoHomeClicked = () => navigate('/dash');
+
+    return (
+        <footer className="dash-footer">
+            {pathname !== '/dash' && (
+                <button
+                    className="dash-footer__button icon-button cursor-pointer"
+                    title="Home"
+                    onClick={onGoHomeClicked}
+                >
+                    <FontAwesomeIcon icon={faHouse} />
+                </button>
+            )}
+            <p>Current User:</p>
+            <p>Status:</p>
+        </footer>
+    );
+};
+
+export default DashFooter;
